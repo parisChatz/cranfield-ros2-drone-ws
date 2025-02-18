d8d38052fadc04584f11006d47c8ede545a2f17e
Paris Chatzithanos <parischatz94@gmail.com>
2025-02-18 11:45:07 +0000

    added bridge in launchfile

--- src/my_drone_sim/launch/my_world.launch.py
+++ src/my_drone_sim/launch/my_world.launch.py
@@ -12,6 +12,7 @@ def generate_launch_description():
     pkg_share = get_package_share_directory("my_drone_sim")
     world_file = os.path.join(pkg_share, "worlds", "drone_scenario.sdf")
     model_file = os.path.join(pkg_share, "models", "x500_gimbal", "model.sdf")
+    bridge_config_file = os.path.join(pkg_share, "config", "bridge_topics.yaml")
 
     # Gazebo Sim command
     gazebo_cmd = ExecuteProcess(cmd=["gz", "sim", "-r", world_file], output="screen")
@@ -20,15 +21,20 @@ def generate_launch_description():
     spawn_drone_cmd = Node(
         package="ros_gz_sim",
         executable="create",
-        arguments=[
-            "-file",
-            model_file,
-            "-x",
-            "0",
-            "-y",
-            "0",
-            "-z",
-            "0.1",
+        arguments=["-file", model_file, "-x", "0", "-y", "0", "-z", "0.1"],
+        output="screen",
+    )
+
+    # ROS-GZ Bridge command with parameter file
+    bridge_cmd = ExecuteProcess(
+        cmd=[
+            "ros2",
+            "run",
+            "ros_gz_bridge",
+            "parameter_bridge",
+            "--ros-args",
+            "-p",
+            f"config_file:={bridge_config_file}",
         ],
         output="screen",
     )
@@ -39,5 +45,8 @@ def generate_launch_description():
             TimerAction(
                 period=1.0, actions=[spawn_drone_cmd]
             ),  # Delay to ensure Gazebo loads first
+            TimerAction(
+                period=2.0, actions=[bridge_cmd]
+            ),  # Slight delay to start the bridge after spawn
         ]
     )
